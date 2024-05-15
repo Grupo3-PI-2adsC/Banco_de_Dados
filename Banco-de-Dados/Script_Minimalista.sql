b64ed49b1dee401ca8a0a4396261f90c3c613f1f
VagnerDiBendetto <vagner.andrade@sptech.school>
2024-05-15 07:53:50 -0300

    Concluindo alterações solicitadas no DER e Script

--- Banco-de-Dados/Script_Minimalista.sql
+++ Banco-de-Dados/Script_Minimalista.sql
@@ -1,78 +1,79 @@
-CREATE DATABASE IF NOT EXISTS netmed;
-USE netmed;
-
-
-DROP TABLE IF EXISTS endereco;
-DROP TABLE IF EXISTS atendente;
-DROP TABLE IF EXISTS dadosTemposReal;
-DROP TABLE IF EXISTS componentes;
-DROP TABLE IF EXISTS maquina;
-DROP TABLE IF EXISTS empresa;
- 
+DROP DATABASE IF EXISTS netmed;
 CREATE DATABASE IF NOT EXISTS netmed;
 USE netmed;
 
 CREATE TABLE IF NOT EXISTS empresa (
     idEmpresa INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(45),
-    cnpj CHAR(14),
-    email VARCHAR(45),
-    senha VARCHAR(45)
+    nomeFantasia VARCHAR(100),
+    razaoSocial VARCHAR(100),
+    apelido VARCHAR(60),
+    cnpj CHAR(14)
 );
 
 CREATE TABLE IF NOT EXISTS endereco (
     idEndereco INT AUTO_INCREMENT PRIMARY KEY,
+    cep CHAR(8),
     rua VARCHAR(60),
     bairro VARCHAR(60),
     cidade VARCHAR(60),
-    estado VARCHAR(60),
-    cep CHAR(8),
+    unidadeFederativa VARCHAR(60),
     numero VARCHAR(20),
     complemento VARCHAR(90),
     fkEmpresa INT,
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
-CREATE TABLE IF NOT EXISTS atendente (
-    idAtendimento INT AUTO_INCREMENT PRIMARY KEY,
-    nome VARCHAR(255),
-    email VARCHAR(255),
-    senha VARCHAR(255),
+CREATE TABLE IF NOT EXISTS usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    tipoUsuario VARCHAR(45),
+    nome VARCHAR(100),
+    email VARCHAR(100),
+    senha VARCHAR(30),
     fkEmpresa INT,
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 CREATE TABLE IF NOT EXISTS maquina (
     idMaquina INT AUTO_INCREMENT PRIMARY KEY,
-    nomeMaquina VARCHAR(45),
+    hostName VARCHAR(45),
     fkEmpresa INT,
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
+CREATE TABLE IF NOT EXISTS tipoComponente (
+	idTipoComponente INT primary key,
+    nomeComponente VARCHAR(45),
+    unidadeMedida VARCHAR(10),
+    metricaEstabelecida DECIMAL(10,10) -- Métricas máximas e minimas?
+);
+
 CREATE TABLE IF NOT EXISTS componentes (
-    fkMaquina INT,
     idComponente INT NOT NULL,
-    tipoComponente VARCHAR(45),
+    fkMaquina INT,
+    fkTipoComponente INT,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
-    PRIMARY KEY (fkMaquina, idComponente), -- Chave primária composta
-    FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
+    PRIMARY KEY (idComponente, fkMaquina, fkTipoComponente), -- Chave primária composta
+    FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+    FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente)
 );
 
 CREATE TABLE IF NOT EXISTS dadosTempoReal (
     idDadosTempoReal INT AUTO_INCREMENT PRIMARY KEY,
-    fkMaquina INT,
     fkComponente INT,
+    fkMaquina INT,
+    fkTipoComponente INT,
     dataHora DATETIME,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
     unidadeMedida VARCHAR(150),
-    FOREIGN KEY (fkMaquina, fkComponente) REFERENCES componentes(fkMaquina, idComponente)
+    FOREIGN KEY (fkComponente, fkMaquina, fkTipoComponente) 
+    REFERENCES componentes(idComponente, fkMaquina, fkTipoComponente)
 );
 
 SELECT * FROM empresa;
 SELECT * FROM endereco;
-SELECT * FROM atendente;
+SELECT * FROM usuario;
 SELECT * FROM maquina;
 SELECT * FROM componentes;
 SELECT * FROM dadosTempoReal;
