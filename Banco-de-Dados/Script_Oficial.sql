9942db43b252c93d9e8703014009c9b995fc5119
MatteusNogueira <matteus.bins@sptech.school>
2024-05-17 03:39:41 -0300

    ajustes bd

--- Banco-de-Dados/Script_Oficial.sql
+++ Banco-de-Dados/Script_Oficial.sql
@@ -9,6 +9,8 @@ CREATE TABLE IF NOT EXISTS empresa (
     apelido VARCHAR(60),
     cnpj CHAR(14)
 );
+insert into empresa values
+	(null, 'Arcos Dourados', 'Fast Food', 'EME GIGANTE', '12345678901234');
 
 CREATE TABLE IF NOT EXISTS endereco (
     idEndereco INT AUTO_INCREMENT PRIMARY KEY,
@@ -24,6 +26,7 @@ CREATE TABLE IF NOT EXISTS endereco (
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
+
 CREATE TABLE IF NOT EXISTS usuario (
     idUsuario INT AUTO_INCREMENT PRIMARY KEY,
     tipoUsuario VARCHAR(45),
@@ -34,19 +37,42 @@ CREATE TABLE IF NOT EXISTS usuario (
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
+insert into usuario values
+	(null, 'recepção', 'Raimunda', 'raimunda@netmet.com' ,'1234', 1);
+
 CREATE TABLE IF NOT EXISTS maquina (
     idMaquina INT AUTO_INCREMENT PRIMARY KEY,
     hostName VARCHAR(45),
+    ativo Boolean,
+    arquitetura int,	
+    validado boolean,
     fkEmpresa INT,
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+CREATE TABLE IF NOT EXISTS servicos(
+	PID int,
+    nome VARCHAR(50),
+    estado boolean,
+    fkMaquina int,
+    foreign key (fkMaquina) references maquina(idMaquina),
+    primary key (PID, fkMaquina)
 );
 
 CREATE TABLE IF NOT EXISTS tipoComponente (
 	idTipoComponente INT primary key,
     nomeComponente VARCHAR(45),
     unidadeMedida VARCHAR(10),
-    metricaEstabelecida DECIMAL(10,10) -- Métricas máximas e minimas?
+    metricaEstabelecida double -- Métricas máximas e minimas?
 );
+INSERT INTO tipoComponente VALUES 
+	(1, 'Sistema operacional', 'bits', null),
+    (2, 'Memoria', 'GiB', 70.0),
+	(3, 'Processador', 'GHz', 2.0),
+	(4, 'Rede', 'Pacotes', 500000.0),
+	(5, 'Disco ', 'Gib', 7988696064.0),
+	(6, 'Volume  ', 'Gib', null),
+	(7, 'Serviços   ', '%', null);
 
 CREATE TABLE IF NOT EXISTS componentes (
     idComponente INT NOT NULL,
@@ -54,27 +80,31 @@ CREATE TABLE IF NOT EXISTS componentes (
     fkTipoComponente INT,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
-    PRIMARY KEY (idComponente, fkMaquina, fkTipoComponente), -- Chave primária composta
+	descrição varchar(200),
+    PRIMARY KEY (idComponente, fkMaquina), -- Chave primária composta
     FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
     FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente)
 );
 
+
 CREATE TABLE IF NOT EXISTS dadosTempoReal (
     idDadosTempoReal INT AUTO_INCREMENT PRIMARY KEY,
-    fkComponente INT,
     fkMaquina INT,
     fkTipoComponente INT,
     dataHora DATETIME,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
-    unidadeMedida VARCHAR(150),
-    FOREIGN KEY (fkComponente, fkMaquina, fkTipoComponente) 
-    REFERENCES componentes(idComponente, fkMaquina, fkTipoComponente)
+    -- unidadeMedida VARCHAR(150),
+    FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente),
+    FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
 );
+
 
 SELECT * FROM empresa;
 SELECT * FROM endereco;
 SELECT * FROM usuario;
 SELECT * FROM maquina;
-SELECT * FROM componentes;
+SELECT * FROM tipoComponente;
 SELECT * FROM dadosTempoReal;
+SELECT * FROM componentes;
+SELECT * FROM componentes;
