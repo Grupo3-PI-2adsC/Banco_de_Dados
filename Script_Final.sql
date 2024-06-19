bacb681afcf655f85129eb9871539b540d5b98f2
MatteusNogueira <matteus.bins@sptech.school>
2024-06-19 13:26:32 -0300

    ajustes banco mysql

--- Script_Final.sql
+++ Script_Final.sql
@@ -1,11 +1,9 @@
--- Usar banco de dados 'mysql' como padrão para garantir que 'netmed' possa ser deletado
-USE mysql;
-
--- Excluir o banco de dados 'netmed' se ele existir
+-- Deletar banco de dados se existir
 DROP DATABASE IF EXISTS netmed;
 
--- Criação do banco de dados se ele não existir
+-- Cria o banco de dados se ele não existir
 CREATE DATABASE IF NOT EXISTS netmed;
+
 USE netmed;
 
 -- Criação da tabela empresa
@@ -17,10 +15,47 @@ CREATE TABLE IF NOT EXISTS empresa (
     cnpj CHAR(14)
 );
 
--- Inserção de dados na tabela empresa
-INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) 
-SELECT 'Arcos Dourados', 'Fast Food', 'EME GIGANTE', '12345678901234'
+-- Criação da tabela usuario
+CREATE TABLE IF NOT EXISTS usuario (
+    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
+    tipoUsuario VARCHAR(45),
+    nome VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
+    senha VARCHAR(30),
+    ativo BOOLEAN,
+    fkEmpresa INT,
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+);
+
+-- 1º Inserção de dados na tabela empresa
+INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj)
+SELECT 'Hospital São Paulo', 'CENTRO MEDICO SÃO PAULO SC LTDA', 'Hospital SP', '12345678901234'
 WHERE NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '12345678901234');
+
+-- 1º Inserção de dados na tabela usuario
+INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa)
+SELECT 'representante', 'Raimunda Neto', 'raimunda@netmed.com', 'Raim@123', TRUE, 1
+WHERE NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmed.com');
+
+-- 2º Inserção de dados na tabela empresa
+INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj)
+SELECT 'CENTRO HOSPITALAR UNIMED', 'UNIMED DE JOINVILLE COOPERATIVA DE TRABALHOS MEDICOS', 'UNIMED', '09876543211234'
+WHERE NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '09876543211234');
+
+-- 2º Inserção de dados na tabela usuario
+INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa)
+SELECT 'representante', 'Cleiton Olivaras', 'cleiton@netmed.com', 'Clei@123', TRUE, 2
+WHERE NOT EXISTS (SELECT * FROM usuario WHERE email = 'cleiton@netmed.com');
+
+-- 3º Inserção de dados na tabela empresa
+INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj)
+SELECT 'HOSPITAL SAO JOSE', 'ASSOCIACAO HOSPITALAR SAO JOSE DE JARAGUA DO SUL', 'SAO JOSE', '09876543210987'
+WHERE NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '09876543210987');
+
+-- 3º Inserção de dados na tabela usuario
+INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa)
+SELECT 'representante', 'Alberto Maverique', 'alberto@netmed.com', 'Albe@123', TRUE, 3
+WHERE NOT EXISTS (SELECT * FROM usuario WHERE email = 'alberto@netmed.com');
 
 -- Criação da tabela endereco
 CREATE TABLE IF NOT EXISTS endereco (
@@ -37,23 +72,6 @@ CREATE TABLE IF NOT EXISTS endereco (
     FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
--- Criação da tabela usuario
-CREATE TABLE IF NOT EXISTS usuario (
-    idUsuario INT AUTO_INCREMENT PRIMARY KEY,
-    tipoUsuario VARCHAR(45),
-    nome VARCHAR(100),
-    email VARCHAR(100) UNIQUE,
-    senha VARCHAR(30),
-    ativo BOOLEAN,
-    fkEmpresa INT,
-    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
-);
-
--- Inserção de dados na tabela usuario
-INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) 
-SELECT 'recepção', 'Raimunda', 'raimunda@netmet.com', '1234', TRUE, 1
-WHERE NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmet.com');
-
 -- Criação da tabela maquina
 CREATE TABLE IF NOT EXISTS maquina (
     idMaquina VARCHAR(45) PRIMARY KEY,
@@ -66,7 +84,7 @@ CREATE TABLE IF NOT EXISTS maquina (
 );
 
 -- Criação da tabela servicos
-CREATE TABLE IF NOT EXISTS servicos(
+CREATE TABLE IF NOT EXISTS servicos (
     PID INT,
     nome VARCHAR(50),
     estado BOOLEAN,
@@ -77,46 +95,88 @@ CREATE TABLE IF NOT EXISTS servicos(
 
 -- Criação da tabela tipoComponente
 CREATE TABLE IF NOT EXISTS tipoComponente (
-    idTipoComponente INT PRIMARY KEY,
+    idTipoComponente INT,
+    fkEmpresa INT,
     nomeComponente VARCHAR(45),
     unidadeMedida VARCHAR(10),
-    metricaEstabelecida FLOAT
+    metricaEstabelecida FLOAT,
+    PRIMARY KEY (fkEmpresa, idTipoComponente),
+    FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
 );
 
 -- Inserção de dados na tabela tipoComponente
-INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida)
-VALUES 
-    (1, 'Sistema operacional', 'bits', NULL),
-    (2, 'Memoria', 'GiB', 70.0),
-    (3, 'Processador', 'GHz', 2.0),
-    (4, 'Rede', 'Pacotes', 500000.0),
-    (5, 'Disco', 'GiB', 7988696064.0),
-    (6, 'Volume', 'GiB', NULL),
-    (7, 'Serviços', '%', NULL)
-ON DUPLICATE KEY UPDATE
-    nomeComponente = VALUES(nomeComponente),
-    unidadeMedida = VALUES(unidadeMedida),
-    metricaEstabelecida = VALUES(metricaEstabelecida);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 1, 1, 'Sistema operacional', 'bits', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 1);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 1, 2, 'Sistema operacional', 'bits', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 1);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 1, 3, 'Sistema operacional', 'bits', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 1);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 2, 1, 'Memoria', 'GiB', 7.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 2);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 2, 2, 'Memoria', 'GiB', 7.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 2);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 2, 3, 'Memoria', 'GiB', 7.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 2);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 3, 1, 'CPU', 'GHz', 13.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 3);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 3, 2, 'CPU', 'GHz', 13.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 3);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 3, 3, 'CPU', 'GHz', 13.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 3);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 4, 1, 'Rede', 'Pacotes', 3818363.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 4);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 4, 2, 'Rede', 'Pacotes', 3818363.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 4);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 4, 3, 'Rede', 'Pacotes', 3818363.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 4);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 5, 1, 'Disco', 'GiB', 700000.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 5);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 5, 2, 'Disco', 'GiB', 700000.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 5);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 5, 3, 'Disco', 'GiB', 700000.0 WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 5);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 6, 1, 'Volume', 'GiB', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 6);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 6, 2, 'Volume', 'GiB', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 6);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 6, 3, 'Volume', 'GiB', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 6);
+
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 7, 1, 'Serviços', '%', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 7);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 7, 2, 'Serviços', '%', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 7);
+INSERT INTO tipoComponente (idTipoComponente, fkEmpresa, nomeComponente, unidadeMedida, metricaEstabelecida)
+SELECT 7, 3, 'Serviços', '%', NULL WHERE NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 7);
 
 -- Criação da tabela fixosRede
 CREATE TABLE IF NOT EXISTS fixosRede (
     idFixosRede INT AUTO_INCREMENT PRIMARY KEY,
+    fkTipoComponente INT,
+    fkEmpresaDados INT,
     fkMaquina VARCHAR(45),
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(255),
-    FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
+    FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+    FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente)
 );
 
 -- Criação da tabela dadosFixos
 CREATE TABLE IF NOT EXISTS dadosFixos (
     idDadosFixos INT AUTO_INCREMENT PRIMARY KEY,
     fkMaquina VARCHAR(45),
+    fkEmpresaDados INT,
     fkTipoComponente INT,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
     descricao VARCHAR(200),
     FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
-    FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente)
+    FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente)
 );
 
 -- Criação da tabela variaveisRede
@@ -148,24 +208,15 @@ CREATE TABLE IF NOT EXISTS dadosTempoReal (
     idDadosTempoReal INT AUTO_INCREMENT PRIMARY KEY,
     fkDadosFixos INT,
     fkMaquina VARCHAR(45),
+    fkEmpresaDados INT,
     fkTipoComponente INT,
     dataHora DATETIME,
     nomeCampo VARCHAR(45),
     valorCampo VARCHAR(150),
     FOREIGN KEY (fkDadosFixos) REFERENCES dadosFixos(idDadosFixos),
-    FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente),
+    FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente),
     FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
 );
 
--- Seleciona todas as máquinas com idMaquina 'matteus-Nitro-AN515-57'
-SELECT * FROM maquina WHERE idMaquina = 'matteus-Nitro-AN515-57';
-
--- Inserção de dados na tabela empresa
-INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) 
-SELECT 'Arcos Dourados', 'Fast Food', 'EME GIGANTE', '12345678901234'
-WHERE NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '12345678901234');
-
--- Inserção de dados na tabela usuario
-INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) 
-SELECT 'recepção', 'Raimunda', 'raimunda@netmet.com', '1234', TRUE, 1
-WHERE NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmet.com');
+-- Exemplo de consulta
+-- SELECT * FROM maquina WHERE idMaquina = 'matteus-Nitro-AN515-57';
--- scriptOficialServer.sql
+++ scriptOficialServer.sql
@@ -25,12 +25,70 @@ BEGIN
     );
 END
 GO
+-- Criação da tabela usuario
+IF OBJECT_ID('usuario', 'U') IS NULL
+BEGIN
+    CREATE TABLE usuario (
+        idUsuario INT IDENTITY(1,1) PRIMARY KEY,
+        tipoUsuario VARCHAR(45),
+        nome VARCHAR(100),
+        email VARCHAR(100) UNIQUE,
+        senha VARCHAR(30),
+        ativo BIT,
+        fkEmpresa INT,
+        FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
+    );
+END
+GO
 
--- Inserção de dados na tabela empresa
+
+-- 1º Inserção de dados na tabela empresa
 IF NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '12345678901234')
 BEGIN
     INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) VALUES
-        ('Arcos Dourados', 'Fast Food', 'EME GIGANTE', '12345678901234');
+        ('Hospital São Paulo', 'CENTRO MEDICO SÃO PAULO SC LTDA', 'Hospital SP', '12345678901234');
+END
+GO
+
+IF NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmet.com')
+BEGIN
+    INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) VALUES
+        ('representante', 'Raimunda Neto', 'raimunda@netmed.com', 'Raim@123', 1, 1);
+END
+GO
+
+
+
+-- 2º Inserção de dados na tabela empresa
+IF NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '09876543211234')
+BEGIN
+    INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) VALUES
+        ('CENTRO HOSPITALAR UNIMED', '	UNIMED DE JOINVILLE COOPERATIVA DE TRABALHOS MEDICOS', 'UNIMED', '09876543211234');
+END
+
+GO-- Inserção de dados na tabela usuario
+IF NOT EXISTS (SELECT * FROM usuario WHERE email = 'cleiton@netmed.com')
+BEGIN
+    INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) VALUES
+        ('representante', 'Cleiton Olivaras', 'cleiton@netmed.com', 'Clei@123', 1, 2);
+END
+GO
+
+
+
+-- 3º Inserção de dados na tabela empresa
+
+IF NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '09876543210987')
+BEGIN
+    INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) VALUES
+        ('HOSPITAL SAO JOSE', '	ASSOCIACAO HOSPITALAR SAO JOSE DE JARAGUA DO SUL', 'SAO JOSE', '09876543210987');
+END
+
+GO-- Inserção de dados na tabela usuario
+IF NOT EXISTS (SELECT * FROM usuario WHERE email = 'alberto@netmet.com')
+BEGIN
+    INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) VALUES
+        ('representante', 'Alberto Maverique', 'alberto@netmed.com', 'Albe@123', 1, 3);
 END
 GO
 
@@ -53,29 +111,6 @@ BEGIN
 END
 GO
 
--- Criação da tabela usuario
-IF OBJECT_ID('usuario', 'U') IS NULL
-BEGIN
-    CREATE TABLE usuario (
-        idUsuario INT IDENTITY(1,1) PRIMARY KEY,
-        tipoUsuario VARCHAR(45),
-        nome VARCHAR(100),
-        email VARCHAR(100) UNIQUE,
-        senha VARCHAR(30),
-        ativo BIT,
-        fkEmpresa INT,
-        FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
-    );
-END
-GO
-
--- Inserção de dados na tabela usuario
-IF NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmet.com')
-BEGIN
-    INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) VALUES
-        ('recepção', 'Raimunda', 'raimunda@netmet.com', '1234', 1, 1);
-END
-GO
 
 -- Criação da tabela maquina
 IF OBJECT_ID('maquina', 'U') IS NULL
@@ -91,7 +126,8 @@ BEGIN
     );
 END
 GO
-
+USE [netmed]
+GO
 -- Criação da tabela servicos
 IF OBJECT_ID('servicos', 'U') IS NULL
 BEGIN
@@ -110,10 +146,13 @@ GO
 IF OBJECT_ID('tipoComponente', 'U') IS NULL
 BEGIN
     CREATE TABLE tipoComponente (
-        idTipoComponente INT PRIMARY KEY,
+        idTipoComponente INT,
+		fkEmpresa int,
         nomeComponente VARCHAR(45),
         unidadeMedida VARCHAR(10),
-        metricaEstabelecida FLOAT
+        metricaEstabelecida FLOAT,
+		Primary key (fkEmpresa, idTipoComponente),
+        FOREIGN KEY (fkEmpresa) REFERENCES empresa(idEmpresa)
     );
 END
 GO
@@ -122,44 +161,58 @@ GO
 -- Verifica se os dados já existem antes de inserir
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 1)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (1, 'Sistema operacional', 'bits', NULL);
+    INSERT INTO tipoComponente VALUES 
+        (1, 1, 'Sistema operacional', 'bits', NULL),
+        (1, 2, 'Sistema operacional', 'bits', NULL),
+        (1, 3, 'Sistema operacional', 'bits', NULL);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 2)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (2, 'Memoria', 'GiB', 70.0);
+    INSERT INTO tipoComponente VALUES 
+        (2, 1,'Memoria', 'GiB', 7.0),
+        (2, 2,'Memoria', 'GiB', 7.0),
+        (2, 3,'Memoria', 'GiB', 7.0);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 3)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (3, 'Processador', 'GHz', 2.0);
+    INSERT INTO tipoComponente  VALUES 
+        (3, 1,'CPU', 'GHz', 13.0),
+        (3, 2,'CPU', 'GHz', 13.0),
+        (3, 3,'CPU', 'GHz', 13.0);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 4)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (4, 'Rede', 'Pacotes', 500000.0);
+    INSERT INTO tipoComponente VALUES 
+        (4, 1,'Rede', 'Pacotes', 3818363.0),
+        (4, 2,'Rede', 'Pacotes', 3818363.0),
+        (4, 3,'Rede', 'Pacotes', 3818363.0);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 5)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (5, 'Disco', 'GiB', 7988696064.0);
+    INSERT INTO tipoComponente VALUES 
+        (5, 1,'Disco', 'GiB', 700000.0),
+        (5, 2,'Disco', 'GiB', 700000.0),
+        (5, 3,'Disco', 'GiB', 700000.0);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 6)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (6, 'Volume', 'GiB', NULL);
+    INSERT INTO tipoComponente VALUES 
+        (6, 1,'Volume', 'GiB', NULL),
+        (6, 2,'Volume', 'GiB', NULL),
+        (6, 3,'Volume', 'GiB', NULL);
 END
 GO
 IF NOT EXISTS (SELECT * FROM tipoComponente WHERE idTipoComponente = 7)
 BEGIN
-    INSERT INTO tipoComponente (idTipoComponente, nomeComponente, unidadeMedida, metricaEstabelecida) VALUES 
-        (7, 'Serviços', '%', NULL);
+    INSERT INTO tipoComponente  VALUES 
+        (7, 1,'Serviços', '%', NULL),
+        (7, 2,'Serviços', '%', NULL),
+        (7, 3,'Serviços', '%', NULL);
 END
 GO
 
@@ -168,10 +221,13 @@ IF OBJECT_ID('fixosRede', 'U') IS NULL
 BEGIN
     CREATE TABLE fixosRede (
         idFixosRede INT IDENTITY(1,1) PRIMARY KEY,
+        fkTipoComponente INT,
+		fkEmpresaDados int,
         fkMaquina VARCHAR(45),
         nomeCampo VARCHAR(45),
         valorCampo VARCHAR(255),
-        FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
+        FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
+        FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente)
     );
 END
 GO
@@ -182,12 +238,13 @@ BEGIN
     CREATE TABLE dadosFixos (
         idDadosFixos INT IDENTITY(1,1) PRIMARY KEY,
         fkMaquina VARCHAR(45),
+		fkEmpresaDados int,
         fkTipoComponente INT,
         nomeCampo VARCHAR(45),
         valorCampo VARCHAR(150),
         descricao VARCHAR(200),
         FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina),
-        FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente)
+        FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente)
     );
 END
 GO
@@ -231,38 +288,21 @@ BEGIN
         idDadosTempoReal INT IDENTITY(1,1) PRIMARY KEY,
         fkDadosFixos INT,
         fkMaquina VARCHAR(45),
+		fkEmpresaDados int,
         fkTipoComponente INT,
         dataHora DATETIME,
         nomeCampo VARCHAR(45),
         valorCampo VARCHAR(150),
         FOREIGN KEY (fkDadosFixos) REFERENCES dadosFixos(idDadosFixos),
-        FOREIGN KEY (fkTipoComponente) REFERENCES tipoComponente(idTipoComponente),
+        FOREIGN KEY (fkTipoComponente, fkEmpresaDados) REFERENCES tipoComponente(fkEmpresa, idTipoComponente),
         FOREIGN KEY (fkMaquina) REFERENCES maquina(idMaquina)
     );
 END
 GO
-select * from maquina where idMaquina = 'matteus-Nitro-AN515-57';
+-- --select * from maquina where idMaquina = 'matteus-Nitro-AN515-57';
 
 
 
 
 
-
-
-
--- Inserção de dados na tabela empresa
-IF NOT EXISTS (SELECT * FROM empresa WHERE cnpj = '12345678901234')
-BEGIN
-    INSERT INTO empresa (nomeFantasia, razaoSocial, apelido, cnpj) VALUES
-        ('Arcos Dourados', 'Fast Food', 'EME GIGANTE', '12345678901234');
-END
-GO
-
--- Inserção de dados na tabela usuario
-IF NOT EXISTS (SELECT * FROM usuario WHERE email = 'raimunda@netmet.com')
-BEGIN
-    INSERT INTO usuario (tipoUsuario, nome, email, senha, ativo, fkEmpresa) VALUES
-        ('recepção', 'Raimunda', 'raimunda@netmet.com', '1234', 1, 1);
-END
-GO
 
